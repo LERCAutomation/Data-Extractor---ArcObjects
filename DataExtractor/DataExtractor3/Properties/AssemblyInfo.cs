0d29128f8ddc2a0f6e48a37693f5f9daa1130cf2
HesterLyonsConsulting <Hester@HesterLyonsConsulting.co.uk>
2018-01-05 17:27:28 +0000

    Added creator to project
    Now properly attributed to me :-)

--- DataExtractor/DataExtractor3/Properties/AssemblyInfo.cs
+++ DataExtractor/DataExtractor3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataExtractor3")]
+[assembly: AssemblyTitle("DataExtractor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataExtractor3")]
+[assembly: AssemblyCompany("Hester Lyons Consulting")]
+[assembly: AssemblyProduct("DataExtractor")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
